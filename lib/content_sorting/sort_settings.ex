35fc6b49b0f3df0dd77c991cb0c4eb1be158d725
Tyler-pierce <tyler.pierce@gmail.com>
2018-01-04 09:10:29 -0800

    Adjustments to the default sort structure

--- lib/content_sorting/sort_settings.ex
+++ lib/content_sorting/sort_settings.ex
@@ -3,5 +3,5 @@ defmodule SmileysData.ContentSorting.SortSettings do
 		Hours to include in each category of sorting where terminator indicates the hour at which termination
 		functionality will be called
 	"""
-	defstruct time_window_new: {2, 4}, time_window_medium: {4, 8}, time_window_long: {8, 12}, time_window_terminator: {71, 72}, frequency_new: "*/20 * * * *", frequency_medium: "*/30 * * * *", frequency_long: "@hourly", frequency_terminator: "@hourly", depletion_ratio_new: 0.05, depletion_ratio_medium: 0.1, depletion_ratio_long: 0.3, depletion_ratio_terminator: 0.000001
+	defstruct time_window_new: {3, 4}, time_window_medium: {5, 7}, time_window_long: {11, 12}, time_window_terminator: {71, 72}, frequency_new: "20 * * * *", frequency_medium: "30 * * * *", frequency_long: "40 * * * *", frequency_terminator: "50 * * * *", depletion_ratio_new: 0.85, depletion_ratio_medium: 0.9, depletion_ratio_long: 0.3, depletion_ratio_terminator: 0.000001
 end
