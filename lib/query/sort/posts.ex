c1d9df7fc40cd90b9f550c7f686110f1de08c1a0
Tyler-pierce <tyler.pierce@gmail.com>
2018-01-06 09:06:43 -0800

    Bug fix for sort decay query

--- lib/query/sort/posts.ex
+++ lib/query/sort/posts.ex
@@ -5,11 +5,12 @@ defmodule SmileysData.Query.Sort.Posts do
   Lower private vote value for posts that fall within an upper and lower time bound by the given
   ratio.
   """
-  def decay(ratio, interval1, interval2) do
+  def decay(ratio, intervalEnd, intervalStart) do
+    # Window Overlaps Method: Post Time - Post Time + 1 Hour overlaps the timeframe for decay
     qry = "
       UPDATE posts SET voteprivate = (voteprivate - ROUND(voteprivate * " <> ratio <> "))
-      WHERE (inserted_at - INTERVAL '8 hours', inserted_at - INTERVAL '8 hours') 
-        OVERLAPS (CURRENT_TIMESTAMP - " <> interval1 <> ", CURRENT_TIMESTAMP - " <> interval2 <> ");
+      WHERE (inserted_at, inserted_at + INTERVAL '1 HOUR') OVERLAPS 
+        (CURRENT_TIMESTAMP - " <> intervalEnd <> ", CURRENT_TIMESTAMP - " <> intervalStart <> ");
     "
 
     case Ecto.Adapters.SQL.query(Repo, qry, []) do
